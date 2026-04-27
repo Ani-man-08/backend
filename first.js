bf78dc8435db2dd27fb332b4503021e618852cfd
Aniruddha <aniruddhmankar110@gmail.com>
2026-04-27 23:50:46 +0530

    update code structure and add figlet dependency

--- first.js
+++ first.js
@@ -1,5 +1,20 @@
+import figlet from "figlet";
+
+figlet("Raju Mankar", function(error, data){
+  if(error){
+    console.log("somehing went wrong")
+    console.log(error)
+    return;
+  }
+  else{
+     console.log(data)
+  }
+ 
+
+
+})
 function sample() {
   console.log("Aniket Mankar")
 }
 
-module.exports = sample;
+// module.exports = sample;
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-require('dotenv').config()
-const express = require('express')
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express'
 const app = express()
 const port = 4000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send("Hi Server is ready")
 })
 
-app.get('/twitter',(req,res) => {
-  res.send('aniruddhamankar')
-})
-
-app.get('/login',(req,res) =>{
-  res.send('<h1>Login Sucessfully</h1>')
+app.get('/twitter', (req, res) => {
+  res.send("This is my twiiter account")
 })
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(` example app listenig on ${port}`)
+ 
 })
+
